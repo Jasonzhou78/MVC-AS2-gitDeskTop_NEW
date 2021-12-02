fca4b35b6076d8f95691c468cc0a5865073fa2e3
Jason <90533925+Jasonzhou78@users.noreply.github.com>
2021-12-01 21:02:30 -0500

    Update CoverTypeController.cs

--- ZhijunsBooks/Areas/Admin/Controllers/CoverTypeController.cs
+++ ZhijunsBooks/Areas/Admin/Controllers/CoverTypeController.cs
@@ -41,23 +41,26 @@ namespace ZhijunsBooks.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-            return View();
+            return View(coverType);
         }
-        //use HTTP POST to define the post-action method
-        #region API CALLS
+        //use HTTP POST to define the post-action method 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(CoverType coverType)
         {
             if (ModelState.IsValid) //checks all validation in the model(e.g. Name required) to increase security
             {
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name)
+                
                 if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(coverType);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(coverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index)); // to see all the covertypes
@@ -65,6 +68,29 @@ namespace ZhijunsBooks.Areas.Admin.Controllers
             return View(coverType);
         }
         //API calls here
+        #region API CALLS
+
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null);
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            var objFromDb = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
         #endregion    
     }
 }
